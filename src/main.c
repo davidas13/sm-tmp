808e0012ea98f3bbb77c76be1a6d389e3583f451
David Satrio <david.agung.satrio@gmail.com>
2026-04-01 08:30:48 +0700

    feat: setup for example play yt audio

--- src/main.c
+++ src/main.c
@@ -1,3 +1,58 @@
+#include <mpv/client.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) { return 0; }
+int main(void)
+{
+    // Create hello world mpv client
+    mpv_handle *mpv = mpv_create();
+
+    if (!mpv)
+    {
+        fprintf(stderr, "Failed to initialize mpv instance\n");
+
+        return EXIT_FAILURE;
+    }
+
+    if (mpv_initialize(mpv) < 0)
+    {
+        fprintf(stderr, "Failed to intialize mpv instance.\n");
+
+        mpv_terminate_destroy(mpv);
+
+        return EXIT_FAILURE;
+    }
+
+    const char *yt_url = "https://www.youtube.com/watch?v=wDchsz8nmbo";
+
+    const char *command[] = {"loadfile", yt_url, NULL};
+
+    if (mpv_command(mpv, command) < 0)
+    {
+        fprintf(stderr, "Failed to run loadfile command.\n");
+
+        mpv_terminate_destroy(mpv);
+
+        return EXIT_FAILURE;
+    }
+
+    printf("Video playing...\n");
+
+    while (true)
+    {
+        mpv_event *mpv_event = mpv_wait_event(mpv, -1);
+
+        if (mpv_event->event_id == MPV_EVENT_SHUTDOWN)
+        {
+            break;
+        }
+    }
+
+    // Shutdown
+    mpv_terminate_destroy(mpv);
+
+    printf("Player close.\n");
+
+    return EXIT_SUCCESS;
+}
