50ff4b83483b359d2592bac54cf06d0757c58cff
David Satrio <david.agung.satrio@gmail.com>
2026-04-02 00:31:18 +0700

    refactor: implement semantic hungarian notation

--- src/main.c
+++ src/main.c
@@ -3,21 +3,22 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-bool is_url(const char *url)
+bool is_url(const char *sz_url)
 {
     regex_t regex;
 
     // Credit: https://stackoverflow.com/a/38608262/8403177
-    const char *regex_pattern =
+    const char *p_regex_pattern =
         "^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w "
         "\\.-]*)*\\/?";
 
     int regex_compile =
-        regcomp(&regex, regex_pattern, REG_EXTENDED | REG_NOSUB);
+        regcomp(&regex, p_regex_pattern, REG_EXTENDED | REG_NOSUB);
 
     if (regex_compile != 0)
     {
@@ -26,16 +27,16 @@ bool is_url(const char *url)
         return false;
     }
 
-    int regex_execute = regexec(&regex, url, 0, NULL, 0);
+    int regex_execute = regexec(&regex, sz_url, 0, NULL, 0);
 
     return regex_execute == 0;
 }
 
-bool is_filepath(const char *filepath)
+bool is_filepath(const char *sz_filepath)
 {
     struct stat PathInfo;
 
-    if (stat(filepath, &PathInfo) != 0)
+    if (stat(sz_filepath, &PathInfo) != 0)
     {
         fprintf(stderr, "Failed to stat file.\n");
 
@@ -61,40 +62,40 @@ int main(int argc, const char *argv[])
         return EXIT_FAILURE;
     }
 
-    const char *filepath_or_yt_url = argv[1];
+    const char *sz_filepath_or_yt_url = argv[1];
 
-    if (!is_url(filepath_or_yt_url) && !is_filepath(filepath_or_yt_url))
+    if (!is_url(sz_filepath_or_yt_url) && !is_filepath(sz_filepath_or_yt_url))
     {
         fprintf(stderr, "Invalid filepath or url.\n");
 
         return EXIT_FAILURE;
     }
 
-    mpv_handle *mpv = mpv_create();
+    mpv_handle *p_mpv = mpv_create();
 
-    if (!mpv)
+    if (!p_mpv)
     {
         fprintf(stderr, "Failed to initialize mpv instance\n");
 
         return EXIT_FAILURE;
     }
 
-    if (mpv_initialize(mpv) < 0)
+    if (mpv_initialize(p_mpv) < 0)
     {
         fprintf(stderr, "Failed to intialize mpv instance.\n");
 
-        mpv_terminate_destroy(mpv);
+        mpv_terminate_destroy(p_mpv);
 
         return EXIT_FAILURE;
     }
 
-    const char *command[] = {"loadfile", filepath_or_yt_url, NULL};
+    const char *a_command[] = {"loadfile", sz_filepath_or_yt_url, NULL};
 
-    if (mpv_command(mpv, command) < 0)
+    if (mpv_command(p_mpv, a_command) < 0)
     {
         fprintf(stderr, "Failed to run loadfile command.\n");
 
-        mpv_terminate_destroy(mpv);
+        mpv_terminate_destroy(p_mpv);
 
         return EXIT_FAILURE;
     }
@@ -103,15 +104,15 @@ int main(int argc, const char *argv[])
 
     while (true)
     {
-        mpv_event *mpv_event = mpv_wait_event(mpv, -1);
+        mpv_event *p_mpv_event = mpv_wait_event(p_mpv, -1);
 
-        if (mpv_event->event_id == MPV_EVENT_SHUTDOWN)
+        if (p_mpv_event->event_id == MPV_EVENT_SHUTDOWN)
         {
             break;
         }
     }
 
-    mpv_terminate_destroy(mpv);
+    mpv_terminate_destroy(p_mpv);
 
     printf("Player close.\n");
 
